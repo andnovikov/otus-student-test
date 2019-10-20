f21bd68cc0a5fa7178431717ae9968b4c45b2220
anovikov <andnovikov@outlook.com>
2019-10-20 20:32:37 +0300

    Spring boot initializr

---
+++ src/main/java/ru/anovikov/learning/otusstudenttest/OtusStudentTestApplication.java
@@ -0,0 +1,13 @@
+package ru.anovikov.learning.otusstudenttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusStudentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtusStudentTestApplication.class, args);
+	}
+
+}
