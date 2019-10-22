175d05f13628c0e6285cddc642f9654ff45229cf
anovikov <andnovikov@outlook.com>
2019-10-22 23:48:43 +0300

    Starting with spring boot.

--- src/main/java/ru/anovikov/learning/otusstudenttest/config/AppConfig.java
+++ src/main/java/ru/anovikov/learning/otusstudenttest/config/AppConfig.java
@@ -1,11 +1,14 @@
 package ru.anovikov.learning.otusstudenttest.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.MessageSource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
+@ConfigurationProperties("application")
 public class AppConfig {
 
     @Bean
