ca83d687b2ff99e44cdc87a9d13470ad8d58f75e
anovikov <andnovikov@outlook.com>
2019-10-26 11:57:48 +0300

    Fixed starting test app.

--- src/main/java/ru/anovikov/learning/otusstudenttest/OtusStudentTestApplication.java
+++ src/main/java/ru/anovikov/learning/otusstudenttest/OtusStudentTestApplication.java
@@ -1,10 +1,12 @@
 package ru.anovikov.learning.otusstudenttest;
 
+import javafx.application.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import ru.anovikov.learning.otusstudenttest.service.TestingService;
@@ -14,13 +16,8 @@ import ru.anovikov.learning.otusstudenttest.service.TestingService;
 public class OtusStudentTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OtusStudentTestApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner test(TestingService testingService){
-		return(args) -> {
-			testingService.startTest();
-		};
+		ApplicationContext context = SpringApplication.run(OtusStudentTestApplication.class, args);
+		TestingService testingService = context.getBean(TestingService.class);
+		testingService.startTest();
 	}
 }
